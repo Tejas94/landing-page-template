54884a6e0b356469268e1f732b92e271f742a994
Tejas Pawar <tejasrpawar007@gmail.com>
2021-04-24 22:30:16 +0530

    build tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,29 @@ gulp.task('concat-less', function(){
         .pipe(gulp.dest('assets/css'));
 });
 
+gulp.task('copy-assets', function(){
+    console.log('Copying the resources into build folder');
+    let src = [
+        "assets/bootstrap/**",
+        "assets/img/**",
+        "assets/js/**",
+        "assets/css/**",
+    ]
+    return gulp.src(src, {base: 'assets'})
+        .pipe(gulp.dest('build/assets'));
+});
+
+gulp.task('copy-view', function(){
+    console.log('Copying the view files');
+    let src = [
+        "index.html"
+    ]
+    return gulp.src(src)
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', gulp.series('copy-view', 'copy-assets'));
+
 gulp.task('watch', function () {
     //add function instead of [] in gulp 4.x for watch
     gulp.watch('assets/less/*.less' , gulp.series('concat-less'));
